1e892e907672b6f1c3909f6604507c85d4c8648b
shunsukeharada <triplane0318@gmail.com>
2021-06-27 06:52:37 +0000

    Fifth Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,27 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books.reverse_order
+    @books = @user.books
   end
-  
+
   def index
      @books = Book.all
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
