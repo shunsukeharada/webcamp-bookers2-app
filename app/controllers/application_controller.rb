f32bd1cae188648e8661fd8bd1a01fa09e7897be
shunsukeharada <triplane0318@gmail.com>
2021-07-02 08:29:44 +0000

    Twelfth Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(id: current_user)
   end
-  
-  
+
+ 
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,12 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
-    flash[:notice] = "You have updated user successfully."
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
