9d7a22d6fca47b937cc26a34171c8ef3117c91d6
shunsukeharada <triplane0318@gmail.com>
2021-07-02 12:45:09 +0000

    Thirtienth Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
-    flash[:notice] = "You have updated book successfully."
+    if @book.update(book_params)
+     redirect_to book_path(@book)
+     flash[:notice] = "You have updated book successfully."
+    else
+     render :edit
+    end
   end
 
   def destroy
