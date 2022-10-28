2fcdd3e84f69bd8934b0e94c1aa8e18fa4631e64
EC2 Default User <ec2-user@ip-172-31-1-160.ap-northeast-1.compute.internal>
2022-10-28 03:17:31 +0000

    6th Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
  root to: 'homes#top'
  get 'home/about', to: 'homes#about'
- devise_for :installs
  devise_for :users
  
  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
--- db/schema.rb
+++ db/schema.rb
@@ -35,18 +35,6 @@ ActiveRecord::Schema.define(version: 2021_07_03_131846) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "installs", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -54,10 +42,10 @@ ActiveRecord::Schema.define(version: 2021_07_03_131846) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.text "introduction"
     t.string "profile_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
