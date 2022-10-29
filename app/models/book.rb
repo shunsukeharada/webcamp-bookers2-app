18a6231e2c036b3a56a3ee64abc650e5071d1832
EC2 Default User <ec2-user@ip-172-31-1-160.ap-northeast-1.compute.internal>
2022-10-29 06:05:21 +0000

    8th commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,15 +6,7 @@ class Book < ApplicationRecord
   
   is_impressionable
   
-  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
-  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
-  scope :created_2days, -> { where(created_at: 2.days.ago.all_day) } 
-  scope :created_3days, -> { where(created_at: 3.days.ago.all_day) } 
-  scope :created_4days, -> { where(created_at: 4.days.ago.all_day) } 
-  scope :created_5days, -> { where(created_at: 5.days.ago.all_day) } 
-  scope :created_6days, -> { where(created_at: 6.days.ago.all_day) } 
-  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
-  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
   
   validates :title, presence: true
   validates :body, presence: true,
@@ -22,6 +14,10 @@ class Book < ApplicationRecord
                    
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+  end
+  
+  def self.past_week_count
+   (0..6).map { |n| created_days_ago(n).count }.reverse
   end
   
 end
